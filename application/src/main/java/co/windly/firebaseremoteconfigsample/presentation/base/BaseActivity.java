9df29605a81492aa9067f8adb5913586867f2044
Tomasz Dzieniak <tomasz.dzieniak@windly.co>
2018-07-13 11:10:03 +0200

    Add animated logo in splash screen.

---
+++ application/src/main/java/co/windly/firebaseremoteconfigsample/presentation/base/BaseActivity.java
@@ -0,0 +1,58 @@
+package co.windly.firebaseremoteconfigsample.presentation.base;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import butterknife.ButterKnife;
+import io.reactivex.disposables.CompositeDisposable;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+  //region Threading
+
+  protected CompositeDisposable disposables = new CompositeDisposable();
+
+  //endregion
+
+  //region Ui
+
+  @LayoutRes
+  protected abstract int getLayout();
+
+  //endregion
+
+  //region Lifecycle
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(getLayout());
+    initializeViews();
+    initializeManagers();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    disposables.clear();
+  }
+
+  //endregion
+
+  //region View Injection
+
+  protected void initializeViews() {
+    ButterKnife.bind(this);
+  }
+
+  //endregion
+
+  //region Managers
+
+  protected void initializeManagers() {
+    // No-op.
+  }
+
+  //endregion
+}
--- application/src/main/java/co/windly/firebaseremoteconfigsample/presentation/splash/SplashActivity.java
+++ application/src/main/java/co/windly/firebaseremoteconfigsample/presentation/splash/SplashActivity.java
@@ -1,17 +1,45 @@
 package co.windly.firebaseremoteconfigsample.presentation.splash;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import butterknife.BindView;
 import co.windly.firebaseremoteconfigsample.R;
+import co.windly.firebaseremoteconfigsample.presentation.base.BaseActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity {
+
+  //region Ui
+
+  @Override
+  protected int getLayout() {
+    return R.layout.activity_splash;
+  }
+
+  //endregion
 
   //region Lifecycle
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_splash);
+    animateLogo();
+  }
+
+  //endregion
+
+  //region Logo
+
+  @BindView(R.id.logo)
+  ImageView logoView;
+
+  private void animateLogo() {
+    final Drawable drawable = logoView.getDrawable();
+    if (drawable instanceof Animatable) {
+      final Animatable animatable = (Animatable) drawable;
+      animatable.start();
+    }
   }
 
   //endregion
